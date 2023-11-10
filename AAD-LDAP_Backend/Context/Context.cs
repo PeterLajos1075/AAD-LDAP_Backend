d7d86a22496794950bb3c53fcc4fae7ac6f33be0
Péter Kovács <peterkovacs1075@gmail.com>
2023-11-10 14:57:27 +0100

    Context

---
+++ AAD-LDAP_Backend/Context/Context.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AAD_LDAP_Backend.Context
+{
+    public class ContextBasic : DbContext
+    {
+        public ContextBasic(DbContextOptions<ContextBasic> opt) : base(opt) { }
+    }
+}
